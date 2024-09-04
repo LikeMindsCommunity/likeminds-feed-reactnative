9751838fb9b109b7dd9b33baa0846862dc66ac9f
Jai Chaudhary <jai@Jais-MacBook-Pro.local>
2024-09-04 14:00:23 +0530

    added public class instead of static class

--- likeminds-feed-reactnative-integration/constants/Styles.ts
+++ likeminds-feed-reactnative-integration/constants/Styles.ts
@@ -24,180 +24,225 @@ interface StylesProps {
   shouldHideSeparator?: boolean;
 }
 
-const isIOS = Platform.OS === "ios" ? true : false;
+interface LMFeedStylesProps {
+  hue?: number;
+  fontColor?: string;
+  primaryColor?: string;
+  secondaryColor?: string;
+  lightBackgroundColor?: string;
+  isDarkTheme?: boolean;
+  primaryDarkTextColor?: string;
+  secondaryDarkTextColor?: string;
+  primaryLightTextColor?: string;
+  secondaryLightTextColor?: string;
+  fontTypes?: {
+    LIGHT?: string;
+    MEDIUM?: string;
+    SEMI_BOLD?: string;
+    BOLD?: string;
+    BLACK?: string;
+  };
+  shouldHideSeparator?: boolean;
+}
 
-export class STYLES {
-  static $IS_DARK_THEME = false;
-  static $SHOULD_HIDE_SEPARATOR = false;
-  static $HUE = 244;
-  static $COLORS = {
-    PRIMARY: "hsl(244, 75%, 59%)",
-    SECONDARY: "hsl(240, 64%, 91%)",
-    TERTIARY: "#ffffff",
-    MSG: "#777e8e",
-    FONT_PRIMARY: "hsl(244, 75%, 59%)",
-    JOINED_BTN: "hsl(222, 22%, 93%)",
-    WHITE: "#ffffff",
-    BLACK: "#000000",
-    THEME: "#5046E5",
-    TEXT_COLOR: "#484F67",
-    LIGHT_GREY: "#e0e0e0",
-    RED: "#ff0000",
-    REPORT_RED: "#FB1609",
-    whiteTextColor: "#ffffff",
-    darkTextColor: "#000000",
-    lightGreyTextColor: "#00000077",
-    darkGreyTextColor: "#00000099",
+const isIOS = Platform.OS === "ios";
+
+export class LMFeedStyles {
+  public $IS_DARK_THEME: boolean;
+  public $SHOULD_HIDE_SEPARATOR: boolean;
+  public $HUE: number;
+  public $COLORS: {
+    PRIMARY: string;
+    SECONDARY: string;
+    TERTIARY: string;
+    MSG: string;
+    FONT_PRIMARY: string;
+    JOINED_BTN: string;
+    WHITE: string;
+    BLACK: string;
+    THEME: string;
+    TEXT_COLOR: string;
+    LIGHT_GREY: string;
+    RED: string;
+    REPORT_RED: string;
+    whiteTextColor: string;
+    darkTextColor: string;
+    lightGreyTextColor: string;
+    darkGreyTextColor: string;
   };
-  static $FONT_SIZES = {
-    XS: 10,
-    SMALL: 12,
-    REGULAR: 13,
-    MEDIUM: 14,
-    LARGE: 16,
-    XL: 18,
-    XXL: 20,
+  public $FONT_SIZES: Record<string, number>;
+  public $FONT_WEIGHT: Record<string, string>;
+  public $FONT_TYPES: {
+    LIGHT?: string;
+    MEDIUM?: string;
+    SEMI_BOLD?: string;
+    BOLD?: string;
+    BLACK?: string;
   };
-  static $FONT_WEIGHT = {
-    LIGHT: "300" as const,
-    REGULAR: "400" as const,
-    MEDIUM: "500" as const,
-    BOLD: "600" as const,
-    BOLDER: "700" as const,
-  };
-  static $FONT_TYPES = {
-    LIGHT: isIOS ? "Helvetica" : "Roboto",
-    MEDIUM: isIOS ? "Helvetica" : "Roboto",
-    SEMI_BOLD: isIOS ? "Helvetica" : "Roboto",
-    BOLD: isIOS ? "Helvetica" : "Roboto",
-    BLACK: isIOS ? "Helvetica" : "Roboto",
-  };
-  static $BACKGROUND_COLORS = {
-    LIGHT: "#ffffff",
-    DARK: "#000000",
-    DARKTRANSPARENT: "#00000088",
-  };
-  static $TEXT_COLOR = {
-    PRIMARY_TEXT_LIGHT: "#000000",
-    SECONDARY_TEXT_LIGHT: "grey",
-    PRIMARY_TEXT_DARK: "#ffffff",
-    SECONDARY_TEXT_DARK: "grey",
-  };
-  static $SHADOWS = {
-    LIGHT: "0 5px 10px rgba(0, 0, 0, 0.1)",
-    MEDIUM: "0 8px 30px rgba(0, 0, 0, 0.3)",
-    HEAVY: "0 30px 60px rgba(0, 0, 0, 0.6)",
-  };
-  static $MARGINS = {
-    XS: 5,
-    SMALL: 10,
-    MEDIUM: 15,
-    LARGE: 20,
-    XL: 25,
-  };
-  static $PADDINGS = {
-    XS: 5,
-    SMALL: 10,
-    MEDIUM: 15,
-    LARGE: 20,
-    XL: 25,
-  };
-  static $AVATAR = {
-    WIDTH: 50,
-    HEIGHT: 50,
-    BORDER_RADIUS: 25,
-  };
-  static $ALIGN_ROW = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  };
-  static $TEXTVIEW_WIDTH = {
-    REGULAR: 240,
-  };
-  static $STATUS_BAR_STYLE = {
-    default: "default",
-    "dark-content": "dark-content",
-    "light-content": "light-content",
-  };
-  static $LMLoaderSizeiOS: 10;
-  static $LMLoaderSizeAndroid: 25;
-  static $POLL_STYLES: PollStyles = {};
-  static setTheme({
-    hue,
-    fontColor,
-    primaryColor,
-    secondaryColor,
-    lightBackgroundColor,
-    isDarkTheme,
-    primaryDarkTextColor,
-    secondaryDarkTextColor,
-    primaryLightTextColor,
-    secondaryLightTextColor,
-    fontTypes,
-    shouldHideSeparator
-  }: StylesProps) {
-    STYLES.$SHOULD_HIDE_SEPARATOR = shouldHideSeparator ? shouldHideSeparator : false
-    STYLES.$COLORS = {
-      ...STYLES.$COLORS,
-      PRIMARY: primaryColor
-        ? primaryColor
-        : `hsl( ${hue ? hue : 244}, 75%, 59%)`,
-      SECONDARY: secondaryColor
-        ? secondaryColor
-        : `hsl(${hue ? hue : 244}, 64%, 91%)`, 
-      FONT_PRIMARY: fontColor
-        ? fontColor      
-        : primaryColor
-        ? primaryColor
-        : `hsl(${hue ? hue : 244}, 75%, 59%)`,
-      JOINED_BTN: lightBackgroundColor
-        ? lightBackgroundColor
-        : `hsl(${hue ? hue : 244}, 22%, 93%)`,
+  public $BACKGROUND_COLORS: Record<string, string>;
+  public $TEXT_COLOR: Record<string, string>;
+  public $SHADOWS: Record<string, string>;
+  public $MARGINS: Record<string, number>;
+  public $PADDINGS: Record<string, number>;
+  public $AVATAR: { WIDTH: number; HEIGHT: number; BORDER_RADIUS: number };
+  public $ALIGN_ROW: Record<string, any>;
+  public $TEXTVIEW_WIDTH: Record<string, number>;
+  public $STATUS_BAR_STYLE: Record<string, string>;
+  public $LMLoaderSizeiOS: number;
+  public $LMLoaderSizeAndroid: number;
+  public $POLL_STYLES: PollStyles;
+
+  constructor({
+    hue = 244,
+    fontColor = "hsl(244, 75%, 59%)",
+    primaryColor = "hsl(244, 75%, 59%)",
+    secondaryColor = "hsl(240, 64%, 91%)",
+    lightBackgroundColor = "#ffffff",
+    isDarkTheme = false,
+    primaryDarkTextColor = "#ffffff",
+    secondaryDarkTextColor = "grey",
+    primaryLightTextColor = "#000000",
+    secondaryLightTextColor = "grey",
+    fontTypes = {
+      LIGHT: isIOS ? "Helvetica" : "Roboto",
+      MEDIUM: isIOS ? "Helvetica" : "Roboto",
+      SEMI_BOLD: isIOS ? "Helvetica" : "Roboto",
+      BOLD: isIOS ? "Helvetica" : "Roboto",
+      BLACK: isIOS ? "Helvetica" : "Roboto",
+    },
+    shouldHideSeparator = false,
+  }: LMFeedStylesProps = {}) {
+    this.$IS_DARK_THEME = isDarkTheme;
+    this.$SHOULD_HIDE_SEPARATOR = shouldHideSeparator;
+    this.$HUE = hue;
+    this.$COLORS = {
+      PRIMARY: primaryColor,
+      SECONDARY: secondaryColor,
+      TERTIARY: "#ffffff",
+      MSG: "#777e8e",
+      FONT_PRIMARY: fontColor,
+      JOINED_BTN: lightBackgroundColor,
+      WHITE: "#ffffff",
+      BLACK: "#000000",
+      THEME: "#5046E5",
+      TEXT_COLOR: "#484F67",
+      LIGHT_GREY: "#e0e0e0",
+      RED: "#ff0000",
+      REPORT_RED: "#FB1609",
+      whiteTextColor: "#ffffff",
+      darkTextColor: "#000000",
+      lightGreyTextColor: "#00000077",
+      darkGreyTextColor: "#00000099",
     };
-    STYLES.$FONT_TYPES = {
-      ...STYLES.$FONT_TYPES,
-      LIGHT: fontTypes?.fontFamilyLight
-        ? fontTypes?.fontFamilyLight
-        : isIOS
-        ? "Helvetica"
-        : "Roboto",
-      MEDIUM: fontTypes?.fontFamilyMedium
-        ? fontTypes?.fontFamilyMedium
-        : isIOS
-        ? "Helvetica"
-        : "Roboto",
-      SEMI_BOLD: fontTypes?.fontFamilySemiBold
-        ? fontTypes?.fontFamilySemiBold
-        : isIOS
-        ? "Helvetica"
-        : "Roboto",
-      BOLD: fontTypes?.fontFamilyBold
-        ? fontTypes?.fontFamilyBold
-        : isIOS
-        ? "Helvetica"
-        : "Roboto",
-      BLACK: fontTypes?.fontFamilyBlack
-        ? fontTypes?.fontFamilyBlack
-        : isIOS
-        ? "Helvetica"
-        : "Roboto",
+    this.$FONT_SIZES = {
+      XS: 10,
+      SMALL: 12,
+      REGULAR: 13,
+      MEDIUM: 14,
+      LARGE: 16,
+      XL: 18,
+      XXL: 20,
     };
-    STYLES.$IS_DARK_THEME = isDarkTheme ? isDarkTheme : false;
-    STYLES.$HUE = hue ? hue : 244;
-    STYLES.$TEXT_COLOR.PRIMARY_TEXT_DARK = primaryDarkTextColor
-      ? primaryDarkTextColor
-      : STYLES.$TEXT_COLOR.PRIMARY_TEXT_DARK;
-    STYLES.$TEXT_COLOR.PRIMARY_TEXT_LIGHT = primaryLightTextColor
-      ? primaryLightTextColor
-      : STYLES.$TEXT_COLOR.PRIMARY_TEXT_LIGHT;
-    STYLES.$TEXT_COLOR.SECONDARY_TEXT_DARK = secondaryDarkTextColor
-      ? secondaryDarkTextColor
-      : STYLES.$TEXT_COLOR.SECONDARY_TEXT_DARK;
-    STYLES.$TEXT_COLOR.SECONDARY_TEXT_LIGHT = secondaryLightTextColor
-      ? secondaryLightTextColor
-      : STYLES.$TEXT_COLOR.SECONDARY_TEXT_LIGHT;
+    this.$FONT_WEIGHT = {
+      LIGHT: "300",
+      REGULAR: "400",
+      MEDIUM: "500",
+      BOLD: "600",
+      BOLDER: "700",
+    };
+    this.$FONT_TYPES = fontTypes;
+    this.$BACKGROUND_COLORS = {
+      LIGHT: "#ffffff",
+      DARK: "#000000",
+      DARKTRANSPARENT: "#00000088",
+    };
+    this.$TEXT_COLOR = {
+      PRIMARY_TEXT_LIGHT: primaryLightTextColor,
+      SECONDARY_TEXT_LIGHT: secondaryLightTextColor,
+      PRIMARY_TEXT_DARK: primaryDarkTextColor,
+      SECONDARY_TEXT_DARK: secondaryDarkTextColor,
+    };
+    this.$SHADOWS = {
+      LIGHT: "0 5px 10px rgba(0, 0, 0, 0.1)",
+      MEDIUM: "0 8px 30px rgba(0, 0, 0, 0.3)",
+      HEAVY: "0 30px 60px rgba(0, 0, 0, 0.6)",
+    };
+    this.$MARGINS = {
+      XS: 5,
+      SMALL: 10,
+      MEDIUM: 15,
+      LARGE: 20,
+      XL: 25,
+    };
+    this.$PADDINGS = {
+      XS: 5,
+      SMALL: 10,
+      MEDIUM: 15,
+      LARGE: 20,
+      XL: 25,
+    };
+    this.$AVATAR = {
+      WIDTH: 50,
+      HEIGHT: 50,
+      BORDER_RADIUS: 25,
+    };
+    this.$ALIGN_ROW = {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+    };
+    this.$TEXTVIEW_WIDTH = {
+      REGULAR: 240,
+    };
+    this.$STATUS_BAR_STYLE = {
+      default: "default",
+      "dark-content": "dark-content",
+      "light-content": "light-content",
+    };
+    this.$LMLoaderSizeiOS = 10;
+    this.$LMLoaderSizeAndroid = 25;
+    this.$POLL_STYLES = {};
+  }
+
+  public setTheme(themeProps: StylesProps) {
+    this.$SHOULD_HIDE_SEPARATOR =
+      themeProps.shouldHideSeparator ?? this.$SHOULD_HIDE_SEPARATOR;
+    this.$COLORS = {
+      ...this.$COLORS,
+      PRIMARY: themeProps.primaryColor ?? this.$COLORS.PRIMARY,
+      SECONDARY: themeProps.secondaryColor ?? this.$COLORS.SECONDARY,
+      FONT_PRIMARY: themeProps.fontColor ?? this.$COLORS.FONT_PRIMARY,
+      JOINED_BTN: themeProps.lightBackgroundColor ?? this.$COLORS.JOINED_BTN,
+    };
+    this.$FONT_TYPES = {
+      ...this.$FONT_TYPES,
+      LIGHT: themeProps.fontTypes?.fontFamilyLight ?? this.$FONT_TYPES.LIGHT,
+      MEDIUM: themeProps.fontTypes?.fontFamilyMedium ?? this.$FONT_TYPES.MEDIUM,
+      SEMI_BOLD:
+        themeProps.fontTypes?.fontFamilySemiBold ?? this.$FONT_TYPES.SEMI_BOLD,
+      BOLD: themeProps.fontTypes?.fontFamilyBold ?? this.$FONT_TYPES.BOLD,
+      BLACK: themeProps.fontTypes?.fontFamilyBlack ?? this.$FONT_TYPES.BLACK,
+    };
+    this.$IS_DARK_THEME = themeProps.isDarkTheme ?? this.$IS_DARK_THEME;
+    this.$HUE = themeProps.hue ?? this.$HUE;
+    this.$TEXT_COLOR.PRIMARY_TEXT_DARK =
+      themeProps.primaryDarkTextColor ?? this.$TEXT_COLOR.PRIMARY_TEXT_DARK;
+    this.$TEXT_COLOR.SECONDARY_TEXT_DARK =
+      themeProps.secondaryDarkTextColor ?? this.$TEXT_COLOR.SECONDARY_TEXT_DARK;
+    this.$TEXT_COLOR.PRIMARY_TEXT_LIGHT =
+      themeProps.primaryLightTextColor ?? this.$TEXT_COLOR.PRIMARY_TEXT_LIGHT;
+    this.$TEXT_COLOR.SECONDARY_TEXT_LIGHT =
+      themeProps.secondaryLightTextColor ??
+      this.$TEXT_COLOR.SECONDARY_TEXT_LIGHT;
+  }
+
+  public setPollStyles(pollStyles: PollStyles) {
+    this.$POLL_STYLES = {
+      ...pollStyles,
+    };
   }
 }
+
+export const STYLES = new LMFeedStyles();
 
 export default STYLES;
