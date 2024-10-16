6d76617a580155ec1b33f14efe413b5cf6059e85
Usman salim <usman.salim@likeminds.community>
2024-10-16 16:29:17 +0530

    added font color for dark theme for checkbox

--- likeminds-feed-reactnative-integration/screens/createPost/index.tsx
+++ likeminds-feed-reactnative-integration/screens/createPost/index.tsx
@@ -1288,17 +1288,17 @@ const CreatePostComponent = () => {
 
 function CheckBox({ isChecked, onPress, label }: any) {
   return (
-      <TouchableOpacity onPress={onPress} style={{flexDirection:'row', gap: 8, justifyContent:'center', alignItems:'center'}}>
-        <View style={{ borderWidth: 1, borderColor: isChecked ? STYLES.$COLORS.PRIMARY : "#D0D5DD", height: 18, width: 18, justifyContent: 'center', alignItems: 'center', borderRadius: 3, backgroundColor: isChecked ? "#D0D5DD" : STYLES.$COLORS.WHITE }}>
-          { isChecked ? <LMIcon
-            assetPath={require("../../assets/images/white_tick3x.png")}
-            color={STYLES.$COLORS.PRIMARY}
-            height={12}
-            width={12}
-          /> : <></>}
-        </View>
-      <Text numberOfLines={2} style={{maxWidth: Layout.normalize(320)}}>{label}</Text>
-      </TouchableOpacity>
+    <TouchableOpacity onPress={onPress} style={{ flexDirection: 'row', gap: 8, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ borderWidth: 1, borderColor: isChecked ? STYLES.$COLORS.PRIMARY : "#D0D5DD", height: 18, width: 18, justifyContent: 'center', alignItems: 'center', borderRadius: 3, backgroundColor: isChecked ? "#D0D5DD" : STYLES.$COLORS.WHITE }}>
+        {isChecked ? <LMIcon
+          assetPath={require("../../assets/images/white_tick3x.png")}
+          color={STYLES.$COLORS.PRIMARY}
+          height={12}
+          width={12}
+        /> : <></>}
+      </View>
+      <Text numberOfLines={2} style={{ maxWidth: Layout.normalize(320), color: STYLES.$IS_DARK_THEME ? STYLES.$COLORS.WHITE_TEXT_COLOR : STYLES.$COLORS.BLACK }}>{label}</Text>
+    </TouchableOpacity>
   )
 }
 
