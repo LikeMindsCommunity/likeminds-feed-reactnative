badc487e2f607455040d423599e1b83b98db1d9a
Usman salim <usman.salim@likeminds.community>
2024-10-27 19:15:32 +0530

    added topics view in customWidget post component

--- qna-feed/screens/PostDetailsScreen.tsx
+++ qna-feed/screens/PostDetailsScreen.tsx
@@ -5,12 +5,15 @@ import {
   UniversalFeedContextProvider,
 } from "@likeminds.community/feed-rn-core";
 import { LMPostContent, LMPostFooter, LMPostHeader, LMPostMedia,  } from "@likeminds.community/feed-rn-core/components";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import LMPostHeading from "@likeminds.community/feed-rn-core/components/LMPost/LMPostHeading";
 import { LINK_ATTACHMENT_TYPE } from "@likeminds.community/feed-rn-core/constants/Strings";
 import LMPostTopResponse from "@likeminds.community/feed-rn-core/components/LMPost/LMPostTopResponse";
 import { useLMPostContext } from "@likeminds.community/feed-rn-core/context";
 import LMPostQnAFeedFooter from "@likeminds.community/feed-rn-core/components/LMPost/LMPostQnAFeedFooter";
+import { useAppSelector } from "@likeminds.community/feed-rn-core/store/store";
+import STYLES from "@likeminds.community/feed-rn-core/constants/Styles";
+import Layout from "@likeminds.community/feed-rn-core/constants/Layout";
 
 const PostDetailsScreen = ({ navigation, route }: any) => {
   return (
@@ -27,18 +30,55 @@ export default PostDetailsScreen;
 
 const LMPostComponent = React.memo(() => {
   const {
-    isHeadingEnabled,
-    isTopResponse,
+    post,
     customFooter,
-    post
+    isHeadingEnabled,
+    isTopResponse
   } = useLMPostContext();
 
-
+  const allTopics = useAppSelector((state: any) => state.feed.topics);
 
   return (
     <View style={{width:'100%',paddingTop: 10, marginBottom: 10}}>
       {/* post header */}
       <LMPostHeader />
+
+      {post?.topics?.length > 0 ? (
+        <View style={{ flexDirection: "row", flexWrap: "wrap", marginTop: 10 }}>
+          {post?.topics?.map((item, index) => {
+            // Find the corresponding topic object from allTopics
+            const topicObject = allTopics[item];
+            return (
+              <View key={index}>
+                <View
+                  style={[
+                    {
+                      backgroundColor: `hsla(${STYLES.$HUE}, 75%, 59%, 0.1)`,
+                      marginLeft: index === 0 ? 15 : 5,
+                      marginTop: Layout.normalize(10),
+                      borderRadius: Layout.normalize(5),
+                    },
+                  ]}
+                >
+                  <Text
+                    style={[
+                      {
+                        fontSize: Layout.normalize(14),
+                        color: STYLES.$COLORS.PRIMARY,
+                        paddingVertical: Layout.normalize(2),
+                        paddingHorizontal: Layout.normalize(8),
+                        fontFamily: STYLES.$FONT_TYPES.LIGHT,
+                      },
+                    ]}
+                  >
+                    {topicObject?.name}
+                  </Text>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      ) : null}
 
       {/* post heading */}
       {isHeadingEnabled ? <LMPostHeading /> : null}
--- qna-feed/screens/UniversalFeedScreen.tsx
+++ qna-feed/screens/UniversalFeedScreen.tsx
@@ -10,7 +10,7 @@ import {
   UniversalFeedContextProvider,
   usePostListContext,
 } from "@likeminds.community/feed-rn-core";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { LMPostContent, LMPostFooter, LMPostHeader, LMPostMedia } from "@likeminds.community/feed-rn-core/components";
 import { useLMPostContext } from "@likeminds.community/feed-rn-core/context";
 import { token } from "../pushNotification";
@@ -19,6 +19,8 @@ import LMPostQnAFeedFooter from "@likeminds.community/feed-rn-core/components/LM
 import { useAppSelector } from "@likeminds.community/feed-rn-core/store/store";
 import LMPostHeading from "@likeminds.community/feed-rn-core/components/LMPost/LMPostHeading";
 import LMPostTopResponse from "@likeminds.community/feed-rn-core/components/LMPost/LMPostTopResponse";
+import Layout from "@likeminds.community/feed-rn-core/constants/Layout";
+import STYLES from "@likeminds.community/feed-rn-core/constants/Styles";
 
 
 const Feed = () => {
@@ -71,10 +73,49 @@ const LMPostComponent = React.memo(() => {
     isTopResponse
   } = useLMPostContext();
 
+  const allTopics = useAppSelector((state: any) => state.feed.topics);
+
   return (
     <View style={{width:'100%',paddingTop: 10, marginBottom: 10}}>
       {/* post header */}
       <LMPostHeader />
+
+      {post?.topics?.length > 0 ? (
+        <View style={{ flexDirection: "row", flexWrap: "wrap", marginTop: 10 }}>
+          {post?.topics?.map((item, index) => {
+            // Find the corresponding topic object from allTopics
+            const topicObject = allTopics[item];
+            return (
+              <View key={index}>
+                <View
+                  style={[
+                    {
+                      backgroundColor: `hsla(${STYLES.$HUE}, 75%, 59%, 0.1)`,
+                      marginLeft: index === 0 ? 15 : 5,
+                      marginTop: Layout.normalize(10),
+                      borderRadius: Layout.normalize(5),
+                    },
+                  ]}
+                >
+                  <Text
+                    style={[
+                      {
+                        fontSize: Layout.normalize(14),
+                        color: STYLES.$COLORS.PRIMARY,
+                        paddingVertical: Layout.normalize(2),
+                        paddingHorizontal: Layout.normalize(8),
+                        fontFamily: STYLES.$FONT_TYPES.LIGHT,
+                      },
+                    ]}
+                  >
+                    {topicObject?.name}
+                  </Text>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      ) : null}
 
       {/* post heading */}
       {isHeadingEnabled ? <LMPostHeading /> : null}
