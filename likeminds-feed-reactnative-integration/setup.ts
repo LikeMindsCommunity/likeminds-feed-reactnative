842181d53ed3797bcbf7c80c2f85ff4755b2dca1
Usman salim <usman.salim@likeminds.community>
2024-10-29 02:18:54 +0530

    version upgrade

--- likeminds-feed-reactnative-integration/setup.ts
+++ likeminds-feed-reactnative-integration/setup.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-rn";
 export const initMyClient = () => {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rn")
-    .setVersionCode(13)
+    .setVersionCode(14)
     .build();
 
   return lmFeedClient;
