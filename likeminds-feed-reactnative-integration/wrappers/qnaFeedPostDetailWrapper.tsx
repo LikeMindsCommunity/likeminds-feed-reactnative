8cea2c8aad11c2c62b2226c4c08d015616b5010c
Usman salim <usman.salim@likeminds.community>
2024-10-28 19:51:33 +0530

    removed default custom widget

--- likeminds-feed-reactnative-integration/wrappers/qnaFeedPostDetailWrapper.tsx
+++ likeminds-feed-reactnative-integration/wrappers/qnaFeedPostDetailWrapper.tsx
@@ -6,18 +6,13 @@ import {
 } from "../context";
 import { PostDetail } from "../screens/postDetail";
 import LMPostQnAFeedFooter from "../components/LMPost/LMPostQnAFeedFooter";
-import { LMPostContent, LMPostFooter, LMPostHeader, LMPostMedia } from "../components";
-import { View } from "react-native";
-import LMPostHeading from "../components/LMPost/LMPostHeading";
-import { LINK_ATTACHMENT_TYPE } from "../constants/Strings";
-import LMPostTopResponse from "../components/LMPost/LMPostTopResponse";
 
 const LMQnAPostDetailScreen = ({ navigation, route }: any) => {
   return (
     <UniversalFeedContextProvider navigation={navigation} route={route}>
       <PostDetailContextProvider navigation={navigation} route={route}>
         {/* @ts-ignore */}
-        <PostDetail isHeadingEnabled={true} lmPostCustomFooter={<LMPostQnAFeedFooter />} customWidgetPostView={<LMPostComponent />}/>
+        <PostDetail isHeadingEnabled={true} lmPostCustomFooter={<LMPostQnAFeedFooter />} />
       </PostDetailContextProvider>
     </UniversalFeedContextProvider>
   );
@@ -25,38 +20,4 @@ const LMQnAPostDetailScreen = ({ navigation, route }: any) => {
 
 export default LMQnAPostDetailScreen;
 
-const LMPostComponent = React.memo(() => {
-  const {
-    isHeadingEnabled,
-    isTopResponse,
-    customFooter,
-    post
-  } = useLMPostContext();
 
-
-
-  return (
-    <View style={{width:'100%',paddingTop: 10, marginBottom: 10}}>
-      {/* post header */}
-      <LMPostHeader />
-
-      {/* post heading */}
-      {isHeadingEnabled ? <LMPostHeading /> : null}
-
-      {/* post content */}
-      {(post?.text ||
-        post?.attachments?.find(
-          (item) => item?.attachmentType === LINK_ATTACHMENT_TYPE
-        )?.attachmentType === LINK_ATTACHMENT_TYPE) && <LMPostContent />}
-
-      {/* post media */}
-      {post?.attachments && post?.attachments.length > 0 && <LMPostMedia />}
-
-      {/* post top response */}
-      {isTopResponse ? <LMPostTopResponse /> : null}
-
-      {/* post footer */}
-      {customFooter ? customFooter : <LMPostFooter />}
-    </View>
-  );
-});
--- likeminds-feed-reactnative-integration/wrappers/qnaFeedScreenWrapper.tsx
+++ likeminds-feed-reactnative-integration/wrappers/qnaFeedScreenWrapper.tsx
@@ -42,7 +42,6 @@ const Feed = () => {
         <PostsList
           items={mappedTopics}
           lmPostCustomFooter={<LMPostQnAFeedFooter />}
-          customWidgetPostView={<LMPostComponent />}
         />
         <LMCreatePostButton customText="ASK QUESTION" />
       </UniversalFeed>
@@ -61,37 +60,3 @@ const QnAFeedWrapper = ({ navigation, route }) => {
 };
 
 export default QnAFeedWrapper;
-
-const LMPostComponent = React.memo(() => {
-  const {
-    post,
-    isHeadingEnabled,
-    isTopResponse,
-    customFooter,
-  } = useLMPostContext();
-
-  return (
-    <View style={{width:'100%',paddingTop: 10, marginBottom: 10}}>
-      {/* post header */}
-      <LMPostHeader />
-
-      {/* post heading */}
-      {isHeadingEnabled ? <LMPostHeading /> : null}
-
-      {/* post content */}
-      {(post?.text ||
-        post?.attachments?.find(
-          (item) => item?.attachmentType === LINK_ATTACHMENT_TYPE
-        )?.attachmentType === LINK_ATTACHMENT_TYPE) && <LMPostContent />}
-
-      {/* post media */}
-      {post?.attachments && post?.attachments.length > 0 && <LMPostMedia />}
-
-      {/* post top response */}
-      {isTopResponse ? <LMPostTopResponse /> : null}
-
-      {/* post footer */}
-      {customFooter ? customFooter : <LMPostFooter />}
-    </View>
-  );
-});
