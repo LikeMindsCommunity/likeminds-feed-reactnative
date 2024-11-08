cdce85091f6cb8b640fe15b54601bafb83216be0
Jai Chaudhary <jai@Jais-MacBook-Pro.local>
2024-11-08 20:02:17 +0530

    removed unnecessary code

--- social-feed/react-native.config.js
+++ social-feed/react-native.config.js
@@ -2,14 +2,5 @@ module.exports = {
   project: {
     ios: {},
     android: {},
-  },
-  dependencies: {
-    'react-native-video': {
-      platforms: {
-        android: {
-          sourceDir: '../node_modules/react-native-video/android-exoplayer',
-        },
-      },
-    },
-  },
+  }
 };
