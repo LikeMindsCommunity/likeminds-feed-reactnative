6573e99ce1412bb7fd9d96de66c0e2e05b354ec3
Jai Chaudhary <jai@Jais-MacBook-Pro.local>
2024-11-08 21:25:43 +0530

    added new versions

--- likeminds-feed-reactnative-integration/setup.ts
+++ likeminds-feed-reactnative-integration/setup.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-rn";
 export const initMyClient = () => {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rn")
-    .setVersionCode(15)
+    .setVersionCode(16)
     .build();
 
   return lmFeedClient;
