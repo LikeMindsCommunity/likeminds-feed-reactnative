3b961b07f5fefc5a0d210e37849463b63bb05799
Jai Chaudhary <jai.chaudhary.1044@gmail.com>
2024-12-10 22:37:11 +0530

    upgraded new versions

--- likeminds-feed-reactnative-integration/setup.ts
+++ likeminds-feed-reactnative-integration/setup.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-rn";
 export const initMyClient = () => {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rn")
-    .setVersionCode(18)
+    .setVersionCode(19)
     .build();
 
   return lmFeedClient;
