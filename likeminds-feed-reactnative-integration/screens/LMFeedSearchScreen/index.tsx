3b6ced9572cf94ceca1dc3a27437beaa737de407
Usman salim <usman.salim@likeminds.community>
2025-01-20 13:44:38 +0530

    added feed search screen and feed context provider

--- likeminds-feed-reactnative-integration/screens/LMFeedSearchScreen/index.tsx
+++ likeminds-feed-reactnative-integration/screens/LMFeedSearchScreen/index.tsx
@@ -1,13 +1,33 @@
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, RefreshControl, FlatList, ActivityIndicator } from 'react-native'
+import React, { useCallback, useLayoutEffect } from 'react'
 import STYLES from "../../constants/Styles"
-import { LMHeader } from '@likeminds.community/feed-rn-core/components'
-import { LMIcon, LMInputText } from '@likeminds.community/feed-rn-core/uiComponents'
+import { LMHeader, LMLoader, LMPost } from '../../components'
+import { LMIcon, LMInputText, LMText } from '../../uiComponents'
 import { SearchPostContextProvider } from '../../context'
-import { useSearchScreenContext } from '../../context/searchScreenContext'
+import { LMPostViewData } from '../../models'
+import { 
+DELETE_POST_MENU_ITEM, EDIT_POST_MENU_ITEM,
+HIDE_POST_MENU_ITEM, NAVIGATED_FROM_POST,
+PIN_POST_MENU_ITEM, POST_TYPE,
+REPORT_POST_MENU_ITEM, UNHIDE_POST_MENU_ITEM,
+UNPIN_POST_MENU_ITEM
+ } from '../../constants/Strings'
+import { Events } from '../../enums/Events'
+import { LMFeedAnalytics } from '../../analytics/LMFeedAnalytics'
+import { Keys } from '../../enums/Keys'
+import { getPostType } from '../../utils/analytics'
+import { useAppDispatch, useAppSelector } from '../../store/store'
+import { SET_FLOW_TO_POST_DETAIL_SCREEN } from '../../store/types/types'
+import { clearPostDetail } from '../../store/actions/postDetail'
+import { POST_DETAIL } from '../../constants/screenNames'
+import LMPostMenu from '../../customModals/LMPostMenu'
+import { autoPlayPostVideo } from '../../store/actions/feed'
+import { DeleteModal, ReportModal } from '../../customModals'
+import { useSearchFeedCustomisableMethodsContext } from '../../context/searchFeedCallbacksContext'
+import { SearchedPostListContextValues, useSearchedPostListContext } from '../../context/searchedPostListContext'
 
-const LMFeedSearchScreen = ({navigation, route}) => {
-    return(
+const LMFeedSearchScreen = ({ navigation, route }) => {
+    return (
         <SearchPostContextProvider navigation={navigation} route={route}>
             <LMFeedSearchScreenComponent navigation={navigation} route={route} />
         </SearchPostContextProvider>
@@ -15,8 +35,217 @@ const LMFeedSearchScreen = ({navigation, route}) => {
 }
 
 const LMFeedSearchScreenComponent = ({ navigation, route }) => {
-    const {searchPostQuery, setSearchPostQuery, onBackArrowPress, onCrossPress} = useSearchScreenContext();
-    console.log("refresh search screen")
+    const dispatch = useAppDispatch();
+    const {
+        searchFeedData,
+        feedFetching,
+        modalPosition,
+        showActionListModal,
+        closePostActionListModal,
+        postLikeHandler,
+        debouncedSaveFunction,
+        savePostHandler,
+        handleLoadMore,
+        renderLoader,
+        showLoader,
+        showDeleteModal,
+        showReportModal,
+        getPostDetail,
+        setShowReportModal,
+        handleDeletePost,
+        onTapCommentCount,
+        debouncedLikeFunction,
+        setSelectedMenuItemPostId,
+        handlePinPost,
+        handleReportPost,
+        handleEditPost,
+        handleHidePost,
+        onTapLikeCount,
+        onOverlayMenuClick,
+        setPostInViewport,
+        searchPostQuery,
+        setSearchPostQuery,
+        onBackArrowPress,
+        onCrossPress
+    }: SearchedPostListContextValues = useSearchedPostListContext();
+    const postListStyle = STYLES.$POST_LIST_STYLE;
+    const loaderStyle = STYLES.$LOADER_STYLE;
+    const {
+        postLikeHandlerProp,
+        savePostHandlerProp,
+        onSelectCommentCountProp,
+        selectEditPostProp,
+        selectPinPostProp,
+        onTapLikeCountProps,
+        handleDeletePostProps,
+        handleReportPostProps,
+        handleHidePostProp,
+        onOverlayMenuClickProp,
+        onSharePostClicked,
+        isHeadingEnabled,
+        isTopResponse,
+        hideTopicsView,
+    } = useSearchFeedCustomisableMethodsContext();
+
+    const onMenuItemSelect = (
+        postId: string,
+        itemId?: number,
+        pinnedValue?: boolean,
+        postDetail?: LMPostViewData
+    ) => {
+        setSelectedMenuItemPostId(postId);
+        if (itemId === PIN_POST_MENU_ITEM || itemId === UNPIN_POST_MENU_ITEM) {
+            selectPinPostProp
+                ? selectPinPostProp(postId, pinnedValue)
+                : handlePinPost(postId, pinnedValue);
+            let event = pinnedValue ? Events.POST_UNPINNED : Events.POST_PINNED;
+            LMFeedAnalytics.track(
+                event,
+                new Map<string, string>([
+                    [Keys.UUID, postDetail?.user?.sdkClientInfo?.uuid],
+                    [Keys.POST_ID, postId],
+                    [Keys.POST_TYPE, getPostType(postDetail?.attachments)],
+                ])
+            );
+        }
+        if (itemId === REPORT_POST_MENU_ITEM) {
+            handleReportPostProps
+                ? handleReportPostProps(postId)
+                : handleReportPost();
+        }
+        if (itemId === DELETE_POST_MENU_ITEM) {
+            handleDeletePostProps
+                ? handleDeletePostProps(true, postId)
+                : handleDeletePost(true);
+        }
+        if (itemId === HIDE_POST_MENU_ITEM || itemId === UNHIDE_POST_MENU_ITEM) {
+            handleHidePostProp ? handleHidePostProp(postId) : handleHidePost(postId);
+        }
+
+        if (itemId === EDIT_POST_MENU_ITEM) {
+            selectEditPostProp
+                ? selectEditPostProp(postId, postDetail)
+                : handleEditPost(postId, postDetail);
+            LMFeedAnalytics.track(
+                Events.POST_EDITED,
+                new Map<string, string>([
+                    [Keys.UUID, postDetail?.user?.sdkClientInfo.uuid],
+                    [Keys.POST_ID, postId],
+                    [Keys.POST_TYPE, getPostType(postDetail?.attachments)],
+                ])
+            );
+        }
+    };
+    const currentVideoId = useAppSelector((state) => state.feed.currentIdOfVideo);
+
+
+
+    const renderItem = useCallback(
+        ({ item, index }: { item: LMPostViewData; index: number }) => {
+            return (
+                <>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={{
+                            backgroundColor: STYLES.$IS_DARK_THEME
+                                ? STYLES.$BACKGROUND_COLORS.DARK
+                                : STYLES.$BACKGROUND_COLORS.LIGHT,
+                        }}
+                        onPress={() => {
+                            dispatch(clearPostDetail() as any);
+                            dispatch({
+                                type: SET_FLOW_TO_POST_DETAIL_SCREEN,
+                                body: { flowToPostDetailScreen: true },
+                            });
+                            navigation.navigate(POST_DETAIL, [
+                                item?.id,
+                                NAVIGATED_FROM_POST,
+                            ]);
+                        }}
+                        key={item?.id}
+                    >
+                        <LMPost
+                            isHeadingEnabled={isHeadingEnabled}
+                            isTopResponse={isTopResponse}
+                            post={item}
+                            // header props
+                            headerProps={{
+                                postMenu: {
+                                    modalPosition: modalPosition,
+                                    modalVisible: showActionListModal,
+                                    onCloseModal: closePostActionListModal,
+                                    onSelected: (postId, itemId) => {
+                                        onMenuItemSelect(postId, itemId, item?.isPinned);
+                                    },
+                                },
+                                onOverlayMenuClick: (event) => {
+                                    onOverlayMenuClickProp
+                                        ? onOverlayMenuClickProp(event, item?.menuItems, item?.id)
+                                        : onOverlayMenuClick(event, item?.id);
+                                },
+                            }}
+                            // footer props
+                            footerProps={{
+                                likeIconButton: {
+                                    onTap: () => {
+                                        postLikeHandlerProp
+                                            ? postLikeHandlerProp(item?.id)
+                                            : postLikeHandler(item?.id);
+                                    },
+                                },
+                                saveButton: {
+                                    onTap: () => {
+                                        savePostHandlerProp
+                                            ? savePostHandlerProp(item?.id, item?.isSaved)
+                                            : savePostHandler(item?.id, item?.isSaved);
+                                    },
+                                },
+                                likeTextButton: {
+                                    onTap: () => {
+                                        onTapLikeCountProps
+                                            ? onTapLikeCountProps(item?.id)
+                                            : onTapLikeCount(item?.id);
+                                    },
+                                },
+                                commentButton: {
+                                    onTap: () => {
+                                        onSelectCommentCountProp
+                                            ? onSelectCommentCountProp(item?.id)
+                                            : onTapCommentCount(item?.id);
+                                    },
+                                },
+                                shareButton: {
+                                    onTap: () => {
+                                        onSharePostClicked ? onSharePostClicked(item?.id) : {};
+                                    },
+                                },
+                            }}
+                            hideTopicsView={hideTopicsView ?? false}
+                        />
+                    </TouchableOpacity>
+                    {!postListStyle.shouldHideSeparator &&
+                        index != searchFeedData.length - 1 ? (
+                        <View
+                            style={{
+                                height: 11,
+                                backgroundColor: STYLES.$IS_DARK_THEME
+                                    ? STYLES.$SEPARATOR_COLORS.DARK
+                                    : STYLES.$SEPARATOR_COLORS.LIGHT,
+                            }}
+                        />
+                    ) : null}
+                </>
+            );
+        },
+        [
+            isHeadingEnabled,
+            isTopResponse,
+            hideTopicsView,
+            searchFeedData
+        ]
+    );
+
+
     return (
         <SafeAreaView style={styles.safeAreaView}>
             <View style={styles.borderBottomView}>
@@ -38,12 +267,90 @@ const LMFeedSearchScreenComponent = ({ navigation, route }) => {
                                 },
                                 onTap: onCrossPress,
                             }}
-                            inputTextStyle={styles.inputTextStyle} 
-                            inputText={searchPostQuery} 
+                            inputTextStyle={styles.inputTextStyle}
+                            inputText={searchPostQuery}
                             onType={setSearchPostQuery} />
                     </View>
                 </View>
             </View>
+            { searchPostQuery?.length > 0 ?
+                feedFetching ? 
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <LMLoader {...loaderStyle?.loader} />
+                </View> :
+                searchFeedData?.length > 0 ?
+                <View>
+                <FlatList
+                    style={postListStyle?.listStyle}
+                    data={searchFeedData}
+                    extraData={searchFeedData}
+                    renderItem={renderItem}
+                    onEndReachedThreshold={0.9}
+                    onEndReached={handleLoadMore}
+                    removeClippedSubviews={true}
+                    keyExtractor={(item) => {
+                        return item?.id?.toString();
+                    }}
+                    ListFooterComponent={renderLoader}
+                    onViewableItemsChanged={({ changed, viewableItems }) => {
+                        if (changed) {
+                            if (viewableItems) {
+                                setPostInViewport(viewableItems?.[0]?.item?.id);
+                            }
+                        }
+                    }}
+                    viewabilityConfig={{ viewAreaCoveragePercentThreshold: 60 }}
+                />
+                </View> :
+                <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
+                    <LMText textStyle={{
+                        fontSize: 16
+                    }}>No Matching Posts Found</LMText>
+                </View> :
+                <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
+                    <LMText textStyle={{
+                        fontSize: 16
+                    }}>Search Your Query</LMText>
+                </View>
+            }
+
+            {/* delete post modal */}
+            {showDeleteModal && (
+                <DeleteModal
+                    visible={showDeleteModal}
+                    displayModal={(visible) => handleDeletePost(visible)}
+                    deleteType={POST_TYPE}
+                    postDetail={getPostDetail()}
+                    navigation={navigation}
+                />
+            )}
+            {/* report post modal */}
+            {showReportModal && (
+                <ReportModal
+                    visible={showReportModal}
+                    closeModal={() => {
+                        dispatch(autoPlayPostVideo(currentVideoId));
+                        setShowReportModal(false);
+                    }}
+                    reportType={POST_TYPE}
+                    postDetail={getPostDetail()}
+                />
+            )}
+            {/* menu list modal */}
+            {showActionListModal && (
+                <LMPostMenu
+                    post={getPostDetail()}
+                    onSelected={(postId, itemId, isPinned) => {
+                        onMenuItemSelect(postId, itemId, isPinned, getPostDetail());
+                    }}
+                    modalPosition={modalPosition}
+                    modalVisible={showActionListModal}
+                    onCloseModal={closePostActionListModal}
+                    menuItemTextStyle={postListStyle?.header?.postMenu?.menuItemTextStyle}
+                    menuViewStyle={postListStyle?.header?.postMenu?.menuViewStyle}
+                    backdropColor={postListStyle?.header?.postMenu?.backdropColor}
+                />
+            )}
         </SafeAreaView>
     )
 }
---
+++ likeminds-feed-reactnative-integration/screens/searchFeed/index.tsx
@@ -0,0 +1,98 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { ReactNode } from 'react'
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { LMMenuItemsViewData, RootStackParamList } from "../../models";
+import { SearchFeedCustomisableMethodsContextProvider } from '../../context/searchFeedCallbacksContext';
+import LMFeedSearchScreen from '../../screens/LMFeedSearchScreen';
+
+interface SearchFeedProps {
+    children?: React.ReactNode;
+    navigation?: NativeStackNavigationProp<RootStackParamList, "UniversalFeed">;
+    route?: {
+        key: string;
+        name: string;
+        params: Array<string>;
+        path: undefined;
+    };
+    isHeadingEnabled?: boolean;
+    isTopResponse?: boolean;
+    postLikeHandlerProp?: (id: string) => void;
+    savePostHandlerProp?: (id: string, saved?: boolean) => void;
+    selectPinPostProp?: (id: string, pinned?: boolean) => void;
+    selectEditPostProp?: (id: string, post: any) => void;
+    onSelectCommentCountProp?: (id: string) => void;
+    onTapLikeCountProps?: (id: string) => void;
+    handleDeletePostProps?: (visible: boolean, postId: string) => void;
+    handleReportPostProps?: (postId: string) => void;
+    handleHidePostProp?: (postId: string) => void,
+    newPostButtonClickProps?: () => void;
+    onOverlayMenuClickProp?: (
+        event: {
+            nativeEvent: { pageX: number; pageY: number };
+        },
+        menuItems: LMMenuItemsViewData[],
+        postId: string
+    ) => void;
+    onTapNotificationBellProp?: () => void;
+    onSharePostClicked?: (id: string) => void;
+    onSubmitButtonClicked?: any;
+    onAddPollOptionsClicked?: any;
+    onPollOptionClicked?: any;
+    hideTopicsView?: boolean;
+}
+
+interface SearchFeedComponentProps {
+    children: ReactNode;
+}
+
+const SearchFeed = ({
+    navigation,
+    route,
+    children,
+    postLikeHandlerProp,
+    savePostHandlerProp,
+    selectPinPostProp,
+    selectEditPostProp,
+    onSelectCommentCountProp,
+    onTapLikeCountProps,
+    handleDeletePostProps,
+    handleReportPostProps,
+    handleHidePostProp,
+    newPostButtonClickProps,
+    onOverlayMenuClickProp,
+    onTapNotificationBellProp,
+    onSharePostClicked,
+    onSubmitButtonClicked,
+    onAddPollOptionsClicked,
+    onPollOptionClicked,
+    isHeadingEnabled = false,
+    isTopResponse = false,
+    hideTopicsView = false
+}: SearchFeedProps) => {
+    return (
+        <SearchFeedCustomisableMethodsContextProvider
+          postLikeHandlerProp={postLikeHandlerProp}
+          savePostHandlerProp={savePostHandlerProp}
+          selectEditPostProp={selectEditPostProp}
+          selectPinPostProp={selectPinPostProp}
+          onSelectCommentCountProp={onSelectCommentCountProp}
+          onTapLikeCountProps={onTapLikeCountProps}
+          handleHidePostProp={handleHidePostProp}
+          handleDeletePostProps={handleDeletePostProps}
+          handleReportPostProps={handleReportPostProps}
+          newPostButtonClickProps={newPostButtonClickProps}
+          onOverlayMenuClickProp={onOverlayMenuClickProp}
+          onTapNotificationBellProp={onTapNotificationBellProp}
+          onSharePostClicked={onSharePostClicked}
+          isHeadingEnabled={isHeadingEnabled}
+          isTopResponse={isTopResponse}
+          hideTopicsView={hideTopicsView}
+        > 
+            <LMFeedSearchScreen navigation={navigation} route={route} />
+        </SearchFeedCustomisableMethodsContextProvider>
+    )
+}
+
+export default SearchFeed
+
+const styles = StyleSheet.create({})
